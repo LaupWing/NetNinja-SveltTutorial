48bde22f43b71bb9ac31d0ac7390166057d2b0f1
LaupWing <loc_strict@hotmail.com>
2021-06-03 11:55:43 +0200

    3

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
-	export let name;
+	let name = 'Laup';
+   const updateName = ()=>{
+      name = 'hi'
+   }
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+   <button on:click={updateName}>Change Name</button>
 </main>
 
 <style>
