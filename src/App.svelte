77820b1542eb707159f65b254bee10b748315cc5
LaupWing <loc_strict@hotmail.com>
2021-06-04 13:28:31 +0200

    19

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script>
-	
+   import Header from './components/Header.svelte'
+   import Footer from './components/Footer.svelte'
 </script>
 
+<Header/>
 <main>
-
+   <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quas, hic
+		dolore deleniti corrupti unde eaque nesciunt repellat sunt nemo assumenda,
+		quasi natus reprehenderit. Consectetur excepturi explicabo inventore sunt
+		reiciendis.
+	</p>
 </main>
+<Footer/>
 
 <style>
-
+	main {
+		width: 100%;
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
