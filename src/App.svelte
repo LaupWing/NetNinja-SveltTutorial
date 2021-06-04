230b12261eb30b072ce6c810a4f758e285624041
LaupWing <loc_strict@hotmail.com>
2021-06-04 13:22:57 +0200

    reset

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,11 @@
 <script>
-	import Modal from './Modal.svelte';
-   import AddPersonForm from './AddPersonForm.svelte'
-	let showModal = false;
-	let people = [
-		{ name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
-		{ name: 'mario', beltColour: 'orange', age: 45, id: 2 },
-		{ name: 'luigi', beltColour: 'brown', age: 35, id: 3 },
-	];
-	const toggleModal = () => {
-		showModal = !showModal;
-	};
-   const addPerson = (e) =>{
-      people = [...people, e.detail]
-      showModal = false
-   }
+	
 </script>
 
-<Modal 
-   isPromo={true} 
-   show={showModal} 
-   on:click={toggleModal}
->
-   <AddPersonForm
-      on:addPerson={addPerson}
-   />
-</Modal>
 <main>
-	<button on:click|once={toggleModal}>Open Modal</button>
-	{#each people as person (person.id)}
-		<div>
-			<h4>{person.name}</h4>
-			{#if person.beltColour === 'black'}
-				<p><strong>Master</strong></p>
-			{/if}
-			<p>{person.age} years old, {person.beltColour} belt.</p>
-		</div>
-	{:else}
-		<p>There are no people to show...</p>
-	{/each}
+
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+
 </style>
