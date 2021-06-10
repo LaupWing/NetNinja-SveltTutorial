c98da5911e2f3f66f7aed24bad92f10182b94cfd
LaupWing <loc_strict@hotmail.com>
2021-06-10 23:07:45 +0200

    21

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
    import Header from './components/Header.svelte'
    import Footer from './components/Footer.svelte'
+   import CreatePollForm from './components/CreatePollForm.svelte'
    import Tabs from './shared/Tabs.svelte'
    
    let items = ['Current Polls', 'Add New Poll']
@@ -13,7 +14,7 @@
    {#if activeItem === 'Current Polls'}
       <p>Current polls component</p>
    {:else if activeItem === 'Add New Poll'}
-      <p>New poll component</p>
+      <CreatePollForm/>
    {/if}
 </main>
 <Footer/>
